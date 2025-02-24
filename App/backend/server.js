40e6a6c549ac55c4d4d36d2e8d0c255eb1b75423
Erik Grinn <grinne@classwork.engr.oregonstate.edu>
2025-02-24 15:59:55 -0800

    not fetching diagnostic

--- App/backend/server.js
+++ App/backend/server.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8500;
 
+
+
 // Middleware:
 
 // If on FLIP, use cors() middleware to allow cross-origin requests from the frontend with your port number:
@@ -14,14 +16,68 @@ app.use(cors({ credentials: true, origin: "*" }));
 app.use(express.json());
 
 // API Routes for backend CRUD:
-app.use("/api/people", require("./routes/peopleRoutes"));
-
+// app.use("/api/diagnostic", require("./routes/diagnosticRoutes"));
 
 // Add your Connect DB Activitiy Code Below:
-// ...
+//...
 
+// define a new GET request with express:
+/*
+    ROUTES
+*/
+// app.get('/', function(req, res)
+//     {
+//         // Define our queries
+//         query1 = 'DROP TABLE IF EXISTS diagnostic;';
+//         query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+//         query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for myONID!")'; //replace with your ONID
+//         query4 = 'SELECT * FROM diagnostic;';
 
-// ...
+//         // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+//         // DROP TABLE...
+//         db.pool.query(query1, function (err, results, fields){
+
+//             // CREATE TABLE...
+//             db.pool.query(query2, function(err, results, fields){
+
+//                 // INSERT INTO...
+//                 db.pool.query(query3, function(err, results, fields){
+
+//                     // SELECT *...
+//                     db.pool.query(query4, function(err, results, fields){
+
+//                         // Send the results to the browser
+//                         let base = "<h1>MySQL Results:</h1>"
+//                         res.send(base + JSON.stringify(results));
+//                     });
+//                 });
+//             });
+//         });
+//     });
+
+// Match to your database config route
+const db = require('./database/config.js')
+
+app.get('/api/diagnostic', async (req, res) => {
+  try {
+    // Await your database queries here
+    await db.pool.query('DROP TABLE IF EXISTS diagnostic;');
+    await db.pool.query('CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);');
+    await db.pool.query('INSERT INTO diagnostic (text) VALUES ("MySQL is working!")');
+    const results = await db.pool.query('SELECT * FROM diagnostic;');
+
+    // res.json() automatically stringifies the JavaScript object to JSON
+    res.json(results);
+
+  } catch (error) {
+    // Handle Errors
+    console.error('Database operation failed:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+//...
 // End Connect DB Activity Code.
 
 
